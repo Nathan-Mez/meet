dc4488a0db451d0beb3e9f9162fdf9ce4d53eaf0
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-08-09 23:57:33 +0300

    Create Event.test.js

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+import { mockData } from '../mock-data';
+
+describe('<Event /> component', () => {
+  let event, EventWrapper;
+  beforeAll(() => {
+    event = mockData[0];
+
+    EventWrapper = shallow(<Event event={event} />);
+  });
+
+  test('render title in event item', () => {
+    expect(EventWrapper.find('.event-title')).toHaveLength(1);
+  });
+
+  test('render info in event item', () => {
+    expect(EventWrapper.find('.event-overview')).toHaveLength(1);
+  });
+
+  test('render show details button in event item', () => {
+    expect(EventWrapper.find('.show-hide-Details-button')).toHaveLength(1);
+  });
+
+  test('render event collapsed by default', () => {
+    expect(EventWrapper.state('show')).toBe(false);
+  });
+
+  test('render click to expand event details', () => {
+    EventWrapper.setState({ show: false});
+    EventWrapper.find('.show-hide-Details-button').simulate('click');
+    expect(EventWrapper.state('show')).toBe(true);
+  });
+
+  test('render with details shown including hide button', () => {
+    EventWrapper.setState({show: true});
+    expect(EventWrapper.find('.event-description').text()).toContain( event.description);
+    expect(EventWrapper.find('.show-hide-Details-button')).toHaveLength(1);
+  });
+
+  test('render click to collapse event details', () => {
+    EventWrapper.setState({show: true});
+    EventWrapper.find('.show-hide-Details-button').simulate('click');
+    expect(EventWrapper.state('show')).toBe(false);
+  });
+
+  test('render with details hidden upon click hide event', () => {
+    EventWrapper.setState({show: false});
+    expect(EventWrapper.find('.event-description')).toHaveLength(0);
+  });
+});
