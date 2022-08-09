66aeef0aea7879bd191b342d8e8564fc24c81256
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-08-09 23:57:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CitySearch />              //city search test box displayed on App component
+        <EventList />               //List of Events on EventList component displayed on App component
+      </div>
+    );
+  }
 }
 
 export default App;
