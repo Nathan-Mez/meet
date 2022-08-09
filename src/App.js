cacb089beaf140c620a8b2638764cb0c13d2a185
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-08-10 01:09:51 +0300

    Add NumberOfEvents to App component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CitySearch />              //city search test box displayed on App component
+        <NumberOfEvents/>           //input box for the number of events user want displayed   
         <EventList />               //List of Events on EventList component displayed on App component
       </div>
     );
