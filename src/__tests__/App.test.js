e66e752a77e758d04847a70fed8c43dec18cf451
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-08-10 01:11:13 +0300

    Update App.test.js to insure NumerOfEvents is rendered

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,6 +4,8 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
+
 
 describe('<App /> component', () => {
   let AppWrapper;
@@ -17,5 +19,9 @@ describe('<App /> component', () => {
 
   test('render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   });
 });
