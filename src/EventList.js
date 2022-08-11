d19ef729a263a38c97148896f0c5f3b5608b758b
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-08-11 12:15:24 +0300

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 import Event from './Event';
 
 class EventList extends Component {
+  
   render() {
     const { events } = this.props;
+
     return (
       <ul className="EventList">
         {events.map(event =>
