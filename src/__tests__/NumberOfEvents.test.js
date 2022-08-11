bdea7426a73d299ef1f0973a4e002d5f7fef583a
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-08-11 22:24:42 +0300

    Update NumberOfEvents.test.js

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -23,7 +23,7 @@ describe('<NumberOfEvents /> component', () => {
     expect(NumberOfEventsWrapper.state('eventCounts')).toBe(10);
   });
 
-  test('If input value is greater than 32 set value to 32', () => {
+  test('If input value is greater than 32 or less than 0 set value to 32', () => {
     NumberOfEventsWrapper.setState({numOfEvents: 32});
     const eventObject = { target: { value: 50 } };
     NumberOfEventsWrapper.find('.event-count-input').simulate('change', eventObject);
