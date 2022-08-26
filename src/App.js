9f17ad1b9b5c0d8546ce52544d2355a483bc0616
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-08-26 10:22:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,15 +29,18 @@ class App extends Component {
     const accessToken = localStorage.getItem('access_token');
     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
+    const code = searchParams.get('code');
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
     if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-   });
-  }
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events)
+          });
+        }
+      });
+    }
   if (navigator.onLine) {
     this.setState({
       offlineText: ''
